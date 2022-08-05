7e324b0c21b3ba84ac634be525477d79926e181a
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2022-08-05 13:03:08 +0100

    F irst commit

---
+++ src/hello.php
@@ -0,0 +1,22 @@
+<!--<html>-->
+<!--<head>-->
+<!--    <title>Hello World Script</title>-->
+<!--</head>-->
+<!--<body>-->
+<!--<p>--><?php //echo 'Hello World'; ?><!--</p>-->
+<!--</body>-->
+<!--</html>-->
+
+<?php
+echo 'Hello World!!';
+echo '<br>';
+?>
+
+<!--1.3 Using variables-->
+
+<?php
+$age = 34;
+
+echo $age;
+//output = 34
+
