c4c997f11cac751ae9e630c7c46f4d9f42b98abf
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2022-08-08 18:31:05 +0100

    End of section 1.3 "using variables" and section 1.4 "single vs double quotes"

--- src/hello.php
+++ src/hello.php
@@ -17,6 +17,34 @@ echo '<br>';
 <?php
 $age = 34;
 
-echo $age;
-//output = 34
+echo $age;  //output = 34
+echo '<br>';
+$age = 35;
+$double_age = $age * 2;
+echo $double_age; //output = 70
+echo '<br>';
+
+define ('VERSION', 1.1);
+echo VERSION; //output 1.1
+echo '<br>';
+
+//1.4 Single vs Double Quotes
+$name = 'Steve';
+echo "$name's Nickname was \"Lugs\"";
+echo '<br>';
+
+$name = 'Steve';
+echo "Hello, $name!";
+echo '<br>';
+
+//String concatenation
+$name = 'Terri';
+echo 'Hello, ' . $name . '!';
+echo '<br>';
+
+echo 'Welcome Steve\'s family!';
+echo '<br>';
+
+
+
 
