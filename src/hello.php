446a6c49e462e09a75d3a1211337852f6a9a3bdf
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2022-08-13 22:21:05 +0100

    Up to 2.4 Ternery Operators

--- src/hello.php
+++ src/hello.php
@@ -162,3 +162,37 @@ $c = $a and $b; //is treated as ($c = $a) and $b which is true
 var_dump($c);
 
 
+//2.3 creating if/else statements
+
+if (10 < 1) {
+    echo '<p>we stopped at if</p>';
+} else if (10 < 4) {
+    echo '<p>we stopped at the first else if.</p>';
+}else if (10<11) {
+    echo '<p>we stopped at the second else if.</p>';
+}else if (10 <20){
+    echo '<p>we stopped at the 3rd else if.</p>';
+}else {
+    echo '<p>None of the conditions are true, we never stopped,
+so this is the else</p>';
+}
+
+$i = 10;
+if (11 == $i) {
+    echo '<h3>Welcome back, user!</h3>';
+}else {
+    echo '<h3>Welcome back to the site!</h3>';
+}
+
+//2.4 Ternery Operators
+$is_logged_in = true;
+
+echo "Welcome" . ($is_logged_in ? " back!" : "!");
+
+$name = null;
+$name = $name ?: 'Joe';
+
+echo '<br>';
+echo $name;
+
+
