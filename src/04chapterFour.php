8b1c533f06d66b9207be23db296e5832c598aa9d
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2022-08-26 13:12:17 +0100

    To end of Chapter Four

--- src/04chapterFour.php
+++ src/04chapterFour.php
@@ -1,6 +1,6 @@
 <?php
 
-// do/while loop
+// 4.2 do/while loop
 $i = 5;
 
 do {
@@ -13,7 +13,7 @@ while ($i < 10 ) {
     $i++;
 }
 
-// for loops
+// 4.3 for loops
 
 //turning while loop above into for loop
 
@@ -30,6 +30,49 @@ for ( $i = 0; $i < sizeof($colours); $i++){
     echo '<pre><h2>' . $colours[$i] . '</pre></h2>';
 
 }
+
+// 4.4 foreach loops
+// using foreach loop with colours array above
+
+foreach ($colours as $colour){
+    echo "<p>$colour</p>";
+}
+
+//Associative Array copied from hello.php for this example
+
+$home_towns = array(
+    'Joe' => 'Middletown, NY',
+    'Erin' =>   'West Chester, PA',
+    'Dave' => 'Exton, PA',
+    'Brian' => 'Grand Rapids, MI',
+);
+
+foreach ($home_towns as $name => $town){
+    echo "<p>$name lives in $town</p>";
+}
+
+//4.5 & 4.6 Fibonacci challenge solution
+
+$current = 1;
+$previous = 0;
+$next = null;
+$limit = 200;
+$sequence = '';
+
+while ( $current < $limit ) {
+
+    $sequence .= $current . ',';
+    $next = $current + $previous;
+    $previous = $current;
+    $current = $next;
+}
+
+$sequence = trim($sequence); // removes whitespace
+$sequence = substr($sequence, 0, strlen($sequence) -1);  //removes last character - ,
+
+echo $sequence;
+
+
 
 
 
