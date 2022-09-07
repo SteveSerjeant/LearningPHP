49871846a34d84a313aa79770ee6ff41b65f3949
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2022-09-07 12:06:57 +0100

    Up to 8.6 RegEx and sanitising input

--- src/process.php
+++ src/process.php
@@ -1,8 +1,21 @@
 <?php
 
+// version from his video
+//foreach ( $_POST as $name => $value){
+//    if ( 'submit' != $name) {
+//        echo "<p><b>" . ucfirst($name) . "</b> is $value</p>";
+//    }
+//}
 
-foreach ( $_POST as $name => $value){
-    if ( 'submit' != $name) {
-        echo "<p><b>" . ucfirst($name) . "</b> is $value</p>";
+//version from the exercise files
+// not sure what the implode does
+// from documentation is used to add separator, but does not seem to work
+// from next video, adds the separator to an array output
+foreach ($_POST as $name=> $value){
+    if ('submit' != $name){
+        if (is_array($value)){
+            $value = implode(', ', $value);
+        }
+        echo "<p><b>" . ucfirst($name) . "</b> is $value.</p>";
     }
 }
