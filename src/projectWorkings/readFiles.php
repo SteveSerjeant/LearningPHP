826468a48c5d81d5b5d09a7c442bd07ffe5397bb
Steve Serjeant <44772548+SteveSerjeant@users.noreply.github.com>
2022-09-22 21:36:34 +0100

    includes demo of reading scan files and extracting just description and ip address

--- src/projectWorkings/readFiles.php
+++ src/projectWorkings/readFiles.php
@@ -1,9 +1,36 @@
 <?php
 
 $text = file_get_contents("C:\Users\sarge\source\xxxx.txt");
-echo $text;
-echo '<br><br>';
+//echo $text;
+//echo '<br><br>';
 
+if ($scan = fopen("C:\Users\sarge\source\xxxx.txt", "r")){
+    while (!feof($scan)){
+        $scanLine = fgets($scan);
+        echo $scanLine, "<br>";
+    }
+
+}
+
+if ($file = fopen("C:\Users\sarge\source\xxxx.txt", "r")){
+    while (!feof($file)){
+        $line = fgets($file);
+//        echo $line, "<br>";
+        if (stristr($line, 'Nmap scan report')) {
+            $output = str_replace('Nmap scan report for', '', $line);
+            $output = str_replace('(', '', $output);
+            $output = str_replace(')', '', $output);
+            echo $output, "<br>";
+
+        }
+
+
+    }
+
+    fclose($file);
+}
+
+echo '<br><br>';
 //$regexIP = '/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(?:\/\d{2})?/';
 $regexIP = '/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/';
 
